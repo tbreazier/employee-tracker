f0e43200f7af3973d81de9414bcf405c535b006c
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-08-09 17:15:07 -0500

    main menu

---
+++ server.js
@@ -0,0 +1,31 @@
+const inquirer = require('inquirer');
+const logo = require('asciiart-logo');
+const { type } = require('os');
+
+const menu = async() => {
+    console.log(logo({ 
+      name: 'Employee Tracker', 
+      logoColor: 'green', 
+      borderColor: 'white'})
+      .render()
+      );
+
+    await inquirer .prompt({
+        name: 'initial',
+        type: 'list', 
+        message: 'What would you like to do?',
+        choices: [
+            'View all employees',
+            'View all departments',
+            'View all roles',
+            'Add department',
+            'Add role',
+            'Add employee',
+            'Update employee role',
+        ],
+
+
+
+    })
+}
+
